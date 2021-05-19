ef87f366b88fbfedd0b94fc8b522f581e788ce91
Muhammad Shahir Rahman <shahir2000@gmail.com>
2021-05-19 16:39:43 -0700

    Code cleanup. Added CNN toy model to config system.

--- project_18408/models/cnn_toy_models.py
+++ project_18408/models/cnn_toy_models.py
@@ -1,8 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 19 16:37:34 2021
+
+@author: Shahir
+"""
+
+import numpy as np
+
 import torch.nn as nn
 import torch.nn.functional as F
-import numpy as np
-import sys
-sys.path.append(r"C:\Users\skarn\OneDrive\Documents\MIT\year_3\18.408 Theoretical Foundations for Deep Learning\generalization-error-final-project")
 
 from project_18408.models.resnet_utils import ResNetBuilder, BasicBlock
 
@@ -22,7 +28,7 @@ class CNNToyModel(ResNetBuilder):
     ):
         super().__init__()
         assert depth > 1
-
+        
         temp_size = input_img_dim
         num_pool = 0
         num_channels = [input_num_channels]
@@ -36,15 +42,12 @@ class CNNToyModel(ResNetBuilder):
                 num_channels.append(num_channels[-1]*2)
             img_shape_per_pool.append(temp_size)
         assert num_pool > 0
-        # max pool, conv stride 2, max pool, conv stride 2, linear
-        """
-        Min 0 blocks per pool
-        Per pool conv stride 2 or max pool
-        Increase num_blocks evenly distributed we reach depth
-        Add extra linear layer as needed since number of layers per block is 2
 
-        """
-        # Between one pool and another, depth = 1 + 2 * num_blocks
+        # Min 0 blocks per pool
+        # Per pool conv stride 2 or max pool
+        # Increase num_blocks evenly distributed we reach depth
+        # Add extra linear layer as needed since number of layers per block is 2
+
         layer_config = [self.MAX_POOL] * num_pool
         current_depth = 1
         index = num_pool - 1
@@ -53,10 +56,7 @@ class CNNToyModel(ResNetBuilder):
             index -= 1
             current_depth += 1
         block_dist = np.linspace(0, (depth-current_depth)//2, num_pool+1).astype(int)
-        print("block_dist=", block_dist)
         block_config = np.diff(block_dist)
-        print("block_config=", block_config)
-        print("layer_config=", layer_config)
         for blocks in block_config:
             current_depth += blocks * 2
         num_linear = 1
@@ -80,7 +80,6 @@ class CNNToyModel(ResNetBuilder):
             
         assert num_linear <= 2
         pre_linear_dim = num_channels[-1] * img_shape_per_pool[-1]**2
-        print(pre_linear_dim)
         layers_fc = []
         if num_linear == 2:   
             layers_fc.append(nn.Linear(pre_linear_dim, pre_linear_dim // 2, bias=True))
@@ -88,13 +87,10 @@ class CNNToyModel(ResNetBuilder):
             layers_fc.append(nn.Linear(pre_linear_dim // 2, output_dim, bias=True))
         else:
             layers_fc.append(nn.Linear(pre_linear_dim, output_dim, bias=True))
-        # print(pre_linear_dim)
         self.layers_conv = nn.Sequential(*layers_conv)
         self.layers_fc = nn.Sequential(*layers_fc)
 
-        # print(layer_config)
-        # self._make_layer(self, in_planes, out_planes, num_blocks, stride=1, block=BasicBlock)
-        pass
+        assert len(self.get_layers_with_weights()) == depth
     
     def forward(self, x):
         x = self.layers_conv(x)
@@ -117,6 +113,4 @@ if __name__ == '__main__':
     from torchsummary import summary
     print(toy_model)
     summary(toy_model, (1, 28, 28))
-    
-    print((toy_model.get_layers_with_weights()))
     print(len(toy_model.get_layers_with_weights()))
